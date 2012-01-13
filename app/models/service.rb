5f0974c3aed7856bb792f9a10a199fff2dda21ad
Greg Signal <greg.signal@trineo.co.nz>
2012-01-13 13:08:20 +1300

    Adds spec for Service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,5 @@
 class Service < ActiveRecord::Base
+
+  has_many :vendors
+
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Service do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Service.new(:name => "foo") }
+
+  it "should have a name" do
+    subject.name.should == "foo"
+  end
+
+  it "should have_many vendors" do
+    subject.vendors.class.should == Array
+  end
+
+  it "should allow vendors to be added and deleted" do
+    expect {
+      new_vendor = subject.vendors.build
+      new_vendor.save!
+    }.should change(subject.vendors, :count).by 1
+
+    expect {
+      existing_vendor = subject.vendors.first
+      existing_vendor.delete
+    }.should change(subject.vendors, :count).by -1
+  end
 end
