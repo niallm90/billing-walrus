f1d84ef9d88895ee8c555cc08ddcfaf734899d52
Greg Signal <greg@awesome.org.nz>
2012-02-11 22:32:54 +1300

    Payment.user proxies to Slice

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,14 +1,15 @@
 class Payment < ActiveRecord::Base
-  belongs_to :user
   belongs_to :slice
+  belongs_to :user
 
-  attr_accessible :amount, :user, :slice
+  attr_accessible :amount, :slice
 
   validates :amount, :numericality => {
                         :greater_than => 0,
                         :only_integer => true
                       }
-  validates :user, :presence => true
   validates :slice, :presence => true
+
+  delegate :user, :to => :slice
 
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -6,15 +6,17 @@ describe Payment do
 
   let(:slice) { FactoryGirl.build(:slice, :user => user) }
 
-  subject { FactoryGirl.build(:payment, :user => user, :slice => slice) }
+  subject { FactoryGirl.build(:payment, :slice => slice) }
 
-  it { should belong_to :user }
   it { should belong_to :slice }
-  it { should validate_presence_of :user }
+  it { should belong_to :user }
   it { should validate_presence_of :slice }
 
+  it "proxies user to slice" do
+    subject.user.should == slice.user
+  end
+
   it "has the correct fields" do
-    subject.user.should == user
     subject.slice.should == slice
     subject.amount.should == 1000
   end
